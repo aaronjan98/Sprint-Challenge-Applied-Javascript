a845d907e2d4b5658a4d4bfdb6b5d987b8e8f025
aaronjan <aaronjan98@gmail.com>
2019-12-06 08:28:59 -0800

    included the axios link in the tabs.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,15 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(response => {
+    console.log(response);
+    response.data.topics.forEach(item => {
+        const newDog = DogCard(item);
+        entryPoint.appendChild(newDog);
+    });
+})
+.catch(error => {
+    console.log("The data was not returned", error);
+});
